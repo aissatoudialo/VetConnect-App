2d0d0cbc5d50100e8a5047bef754fecd2fbf5fa0
abbouteimane34-collab <abbouteimane34@gmail.com>
2026-05-16 17:22:38 +0100

    Update Rapport_animal.php

--- Rapport_animal.php
+++ Rapport_animal.php
@@ -1,1 +1,13 @@
+<?php
 
+// Informations Animal
+$animal = [
+    "nom" => "Simba",
+    "espece" => "Lion",
+    "age" => "5 ans",
+    "date_entree" => "10/05/2026",
+    "date_sortie" => "15/05/2026",
+    "soigneur" => "Jean Dupont"
+];
+
+?>
